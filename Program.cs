47e972604e72c4857c04971f563fb032843314b9
saranbabu29 <sharanra982@gmail.com>
2022-07-31 12:54:56 +0530

    EmployeeWage

--- Program.cs
+++ Program.cs
@@ -7,13 +7,15 @@ namespace Day4EmployeeWage
         static void Main(string[] args)
         {
             int EmpPresent = 1;
+            int Wage_per_hour = 20;
+            int Emphr = 8;
 
             Random random = new Random();
             int value = random.Next(0,2);
 
             if (value == EmpPresent)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is Present and Wage is " +(Emphr*Wage_per_hour));
             }
             else
             {
